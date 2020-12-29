1e21fe076b9f0bd1cbf18b71c0289c260d398a68
YoshimasaToguchiR <yoshitogu@gmail.com>
2020-12-28 19:33:09 -0500

    insercion de codigo

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace primerrepo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escribe el num A:");
+            var a = Convert.ToInt32( Console.ReadLine());
+             Console.WriteLine("Escribe el num B:");
+            var b = Convert.ToInt32( Console.ReadLine());
+            var c = a + b;
+            Console.WriteLine("La suma es: "+ c);
+        }
+    }
+}
